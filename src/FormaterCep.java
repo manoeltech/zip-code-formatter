b738d3fe19649d165dae343ed46945fb80bab86e
manoeltech <manoel.programador21@gmail.com>
2023-08-26 19:25:22 -0300

    Create method formatar cep

--- src/FormaterCep.java
+++ src/FormaterCep.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class FormaterCep {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("What your CEP?");
+            String cep = scanner.nextLine();
+
+            String cepFormatado = formatarcep(cep);
+
+            System.out.println("CEP formatted: " + cepFormatado);
+
+            scanner.close();
+        } catch (CepInvalidException e){
+            System.out.println("Accept only 8 digits");
+        }
+    }
+
+    public static String formatarcep (String cep) throws CepInvalidException {
+        if (cep.length() != 8)
+        throw new CepInvalidException();
+        return cep.substring(0, 5).concat("-").concat(cep.substring(6, 8));
     }
 }
